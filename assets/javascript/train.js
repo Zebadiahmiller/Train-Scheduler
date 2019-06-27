164fcf46ff34d15474fb17ffa5335dd678ec001b
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-06-27 12:38:16 -0700

    initial commit with html and firebase initializer in place

---
+++ assets/javascript/train.js
@@ -0,0 +1,14 @@
+//firebase initializer
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC7P0uoiwstmbIKLxgEZYnCY4lLrHR1RT4",
+    authDomain: "first-play-around.firebaseapp.com",
+    databaseURL: "https://first-play-around.firebaseio.com",
+    projectId: "first-play-around",
+    storageBucket: "first-play-around.appspot.com",
+    messagingSenderId: "744710407073",
+    appId: "1:744710407073:web:48efe4100bc1e755"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
